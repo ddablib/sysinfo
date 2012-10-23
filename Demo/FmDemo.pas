216f96ba3388a637ff7077cbd48d82b3c5049494
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2012-10-23 22:55:07 +0000

    sysinfo: * Changed to explicit public domain license.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1007 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- Demo/FmDemo.pas
+++ Demo/FmDemo.pas
@@ -6,9 +6,8 @@
  * $Rev$
  * $Date$
  *
- * This file is copyright (C) P D Johnson (www.delphidabbler.com), 2003-2012.
- * It may be used without restriction. This code distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * Any copyright in this file is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
 }
 
 

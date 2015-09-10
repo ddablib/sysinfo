e1e84e2fa8d82e172671cf4730a92862175e2289
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2015-09-10 16:40:27 +0000

    sysinfo: * Dedicated Release.bat to Public Domain.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1992 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- DevTools/Release.bat
+++ DevTools/Release.bat
@@ -2,7 +2,8 @@
 @rem Script used to create zip file containing source code of System Information
 @rem Classes.
 @rem
-@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2008-2009
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
 @rem
 @rem $Rev$
 @rem $Date$

978883ec742f6a55d4014a02c1b6ba8226b9714a
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2022-06-01 15:33:15 +0100

    Update Release.bat
    * to take a version number as parameter to use in the release file name
    * to use the correct file in the release, following renaming.
    * re change from SVN Git
    * to permit `Zip.exe` to be called with no directory name when on the path.

--- DevTools/Release.bat
+++ DevTools/Release.bat
@@ -6,14 +6,18 @@
 @rem
 @rem 1) This script uses the zip.exe program to create the release zip file.
 @rem
-@rem 2) The ZIPPath environment variable must exist and store the path to the
-@rem    directory where zip.exe is located. ZIPPATH must not have a trailing
-@rem    backslash.
+@rem 2) If the ZIPPATH environment variable exists it must provide the path to
+@rem    the directory where zip.exe is located. ZIPPATH *must not* have a
+@rem    trailing backslash. If ZIPPATH does not exist then Zip.exe is expected
+@rem    to be on the path.
+@rem
+@rem 3) A release version number may be provided as a parameter to the script.
+@rem    When present the version number is included in the name of the zip file
+@rem    that is created.
 @rem
 @rem Any copyright in this file is dedicated to the Public Domain.
 @rem http://creativecommons.org/publicdomain/zero/1.0/
 @rem ---------------------------------------------------------------------------
-
 
 @echo off
 
@@ -21,24 +25,29 @@ setlocal
 
 cd ..
 
-set OutFile=Release\dd-sysinfo.zip
 set SrcDir=
 set DocsDir=Docs
 set DemoDir=Demos
 
+set OutFile=Release\dd-sysinfo
+if not "%1"  == "" set OutFile=%OutFile%-%1
+set OutFile=%OutFile%.zip
+echo Output file name = %OutFile%
+if exist %OutFile% del %OutFile%
+
+if not "%ZIPPATH%" == "" set ZIPPATH=%ZIPPATH%\
+echo Zip path = %ZIPPATH%
+
 if exist Release rmdir /S /Q Release
 mkdir Release
 
-echo "OUTFILE=%OutFile%"
-if exist %OutFile% del %OutFile%
+%ZIPPATH%Zip.exe -j -9 %OutFile% PJSysInfo.pas
 
-%ZIPPATH%\Zip.exe -j -9 %OutFile% PJSysInfo.pas
+%ZIPPATH%Zip.exe -j -9 %OutFile% CHANGELOG.md
+%ZIPPATH%Zip.exe -j -9 %OutFile% README.md
+%ZIPPATH%Zip.exe -j -9 %OutFile% %DocsDir%\MPL-2.0.txt
+%ZIPPATH%Zip.exe -j -9 %OutFile% %DocsDir%\Documentation.URL
 
-%ZIPPATH%\Zip.exe -j -9 %OutFile% %DocsDir%\ChangeLog.txt
-%ZIPPATH%\Zip.exe -j -9 %OutFile% %DocsDir%\MPL-2.0.txt
-%ZIPPATH%\Zip.exe -j -9 %OutFile% %DocsDir%\ReadMe.htm
-%ZIPPATH%\Zip.exe -j -9 %OutFile% %DocsDir%\Documentation.URL
-
-%ZIPPATH%\Zip.exe %OutFile% -r -9 %DemoDir%\*.* -x *.svn\*
+%ZIPPATH%Zip.exe %OutFile% -r -9 %DemoDir%\*.*
 
 endlocal
